3f472d6e4ca3e79c2ff6ea6a44a491d247b76d30
ajith49 <84385755+ajith49@users.noreply.github.com>
2021-08-21 12:52:33 +0530

    Add files via upload

---
+++ Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner() {
+    return (
+        <div>
+            <h1>Thankyou letsupgrade for ths amazing course</h1>
+        </div>
+    )
+}
+
+export default Banner;
